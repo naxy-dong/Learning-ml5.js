c80229eae6dbe75833bf7b5295f8dc8c7a549c4a
YuXiang Dong <yuxiang.dong@stonybrook.edu>
2022-12-31 23:17:31 -0500

    Added object detector to the list. Was able to incorporate p5 canvas into the image. Also centered the image vertically.

---
+++ object_detector/itemdata.js
@@ -0,0 +1,14 @@
+pageTitle = "ml5.js: Object Detector using mobilenet"; 
+
+itemList = [
+{itemName:"Rax", itemInfo:"Rax was a dragon scout who was betrayed by one of his own. Killed before his time,Rax returned as a skeleton to continue the fight.", itemImage:"Rax.png"},
+{itemName: "Ash", itemInfo:"Horn in flame, Ash uses the heat of his body to wound his enemies.", itemImage:"Ash.png"}, 
+{itemName:"Barnabas", itemInfo: "Barnabas is the oldest and most respected of the dragons. He is wise beyond mortal years.", itemImage:"Barnabas.png"}, 
+{itemName:"Drake", itemInfo:"Master of the flame, Drake can firea molten jet at distant enemies.", itemImage:"Drake.png"}, 
+{itemName:"Endisun", itemInfo: "With electricity coursing through his body at all times, Endisun is not only a deadly foe, but he can shoot into and out of the field of battle without his opponents even noticing.", itemImage:"Endisun.png"}, 
+{itemName:"Lunar", itemInfo:"Lunar's magical healing abilities makes her a formidable opponent.", itemImage:"Lunar.png"},
+{itemImage:"axolot.jpg"},
+{itemImage:"house.jpg"},
+{itemImage:"asriel.jpg"},
+{itemImage:"undertale.jpg", itemInfo:"t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like"}
+];
---
+++ object_detector/menu.js
@@ -0,0 +1,102 @@
+let detector
+let myImg;
+let label
+let confidence
+let fullsrc
+let canvas_div 
+let preloadTime
+let ratio
+
+/*************************ML5.js****************************/
+function modelReady() {
+    console.log("Model is ready!")
+}
+
+function detect() {
+    detector.detect(myImg, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        for(let i = 0; i < results.length; i++){
+            let obj = result[i];
+            stroke(0,255,0);
+            strokeWeight(4);
+            noFill();
+            rect(obj.x, obj.y, obj.width, obj,height)
+            // label.innerText = `Label: ${results[0].label}, ${results[1].label},  ${results[2].label}`
+            // confidence.innerText = 'Confidence: ' + nf(results[0].confidence, 0, 2);
+            console.log(obj);
+        }
+    }
+}
+
+/*************************Interface/UI/p5 stuff****************************/
+function preload() {
+    detector = ml5.objectDetector('cocossd', modelReady)
+
+    captionOutput = document.getElementById("caption");
+    infoOutput = document.getElementById("info");
+    itemThumbs = document.getElementsByClassName("thumb");
+    label = document.getElementById("label");
+    confidence = document.getElementById("confidence");
+    document.getElementById("pagetitle").innerHTML = pageTitle;
+    currentItemIdx = 0;
+    offset = 0;
+    preloadTime = true;
+    display();
+}
+
+function display() {
+    relativesrc = itemList[currentItemIdx].itemImage;
+    fullsrc = "../assets/images/" + relativesrc;
+    if(!preloadTime){
+        drawImage();
+    }
+    preloadTime = false;
+
+    captionOutput.innerHTML = itemList[currentItemIdx].itemName == null ? relativesrc.substring(0, relativesrc.lastIndexOf(".")) : itemList[currentItemIdx].itemName;
+    infoOutput.innerHTML = itemList[currentItemIdx].itemInfo == null ? "no information found" : itemList[currentItemIdx].itemInfo;
+    label.innerText = "Label: "
+    confidence.innerText = "Confidence: "
+    for (var i = 0; i < itemThumbs.length; i++)
+        itemThumbs[i].src = "../assets/images/" + itemList[i + offset].itemImage;
+}
+
+function setup(){
+    canvas_div = document.getElementById("canvas-div")
+    canvas = createCanvas(canvas_div.clientWidth, canvas_div.clientHeight);
+    canvas.parent("canvas-div");
+    drawImage();
+}
+
+//to keep the ratio of the image properly, also draws it in the center
+function drawImage(){
+    myImg = loadImage(fullsrc, () =>{
+        ratio = myImg.width / myImg.height;
+        myImg.resize(canvas_div.clientWidth, canvas_div.clientWidth / ratio);
+        resizeCanvas(myImg.width, canvas_div.clientHeight);
+        image(myImg, 0,(canvas_div.clientHeight - myImg.height)/2);
+    })
+}
+
+function windowResized() {
+    drawImage();
+}
+
+function moveOffset(mod) {
+    offset += mod;
+    if (offset < 0)
+        offset = 0;
+    if (offset >= itemList.length - 3)
+        offset = itemList.length - 3;
+    display();
+}
+function selectItem(idx) {
+    currentItemIdx = idx + offset;
+    display();
+}
+
