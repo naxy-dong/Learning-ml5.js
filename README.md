ab53892fb8ee14ab51e1254565695c8c6c832c82
YuXiang Dong <yuxiang.dong@stonybrook.edu>
2023-01-08 21:06:10 -0500

    Complete Object Detection website.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Learning-ml5.js
-This is my learning for ml5.js in preparation of robotics team.
+This is my learning for ml5.js in preparation of robotics team. The website covers several models of the ml5 library which I tried out.
--- object_detector/menu.js
+++ object_detector/menu.js
@@ -1,10 +1,8 @@
-pageTitle = "ml5.js: Object Detector using mobilenet"; 
+pageTitle = "ml5.js: Object Detector using mobilenet";
 let detector
 let myImg;
-let label
-let confidence
 let fullsrc
-let canvas_div 
+let canvas_div
 let preloadTime
 let ratio
 
@@ -21,26 +19,35 @@ function detect() {
 function gotResult(error, results) {
     if (error) {
         console.log(error)
+        return;
     }
-    else {
-        console.log("detected");
-        console.log(results);
+    console.log("detected");
+    console.log(results);
 
-        if(results.length == 0){
-            label.innerText = "Nothing Detected";
-            confidence.innerText = 'Confidence: 100%';
-        }
+    document.getElementById("detectMsg").innerHTML = results.length == 0 ?
+    "Nothing Detected": "Object Detected";
 
-        for(let i = 0; i < results.length; i++){
-            let obj = results[i];
-            stroke(0,255,0);
-            strokeWeight(4);
-            noFill();
-            rect(obj.x, obj.y + (canvas_div.clientHeight - myImg.height)/2, obj.width, obj.height)
-            // label.innerText = `Label: ${results[0].label}, ${results[1].label},  ${results[2].label}`
-            // confidence.innerText = 'Confidence: ' + nf(results[0].confidence, 0, 2);
-            console.log(obj);
-        }
+    table = document.getElementById("detection-table")
+
+    for (let i = 0; i < results.length && i < 3; i++) {
+        let obj = results[i];
+        stroke(0, 255, 0);
+        strokeWeight(4);
+        noFill();
+        rect(obj.x, obj.y + (canvas_div.clientHeight - myImg.height) / 2, obj.width, obj.height)
+        noStroke();
+        fill(222,222,222);
+        textStyle(BOLD);
+        textSize(32);
+        text(obj.label, obj.x + 20, obj.y + (canvas_div.clientHeight - myImg.height) / 2 + 40);
+
+        let newRow = table.insertRow();
+        let newCell;
+        
+        newCell = newRow.insertCell();
+        newCell.innerHTML = obj.label;
+        newCell = newRow.insertCell();
+        newCell.innerHTML = obj.confidence * 100 + "%";
     }
 }
 
@@ -50,8 +57,6 @@ function preload() {
     captionOutput = document.getElementById("caption");
     infoOutput = document.getElementById("info");
     itemThumbs = document.getElementsByClassName("thumb");
-    label = document.getElementById("label");
-    confidence = document.getElementById("confidence");
     document.getElementById("pagetitle").innerHTML = pageTitle;
     currentItemIdx = 0;
     offset = 0;
@@ -62,20 +67,18 @@ function preload() {
 function display() {
     relativesrc = itemList[currentItemIdx].itemImage;
     fullsrc = "../assets/images/" + relativesrc;
-    if(!preloadTime){
+    if (!preloadTime) {
         drawImage();
     }
     preloadTime = false;
 
     captionOutput.innerHTML = itemList[currentItemIdx].itemName == null ? relativesrc.substring(0, relativesrc.lastIndexOf(".")) : itemList[currentItemIdx].itemName;
     infoOutput.innerHTML = itemList[currentItemIdx].itemInfo == null ? "no information found" : itemList[currentItemIdx].itemInfo;
-    label.innerText = "Label: "
-    confidence.innerText = "Confidence: "
     for (var i = 0; i < itemThumbs.length; i++)
         itemThumbs[i].src = "../assets/images/" + itemList[i + offset].itemImage;
 }
 
-function setup(){
+function setup() {
     canvas_div = document.getElementById("canvas-div")
     canvas = createCanvas(canvas_div.clientWidth, canvas_div.clientHeight);
     canvas.parent("canvas-div");
@@ -84,12 +87,12 @@ function setup(){
 }
 
 //to keep the ratio of the image properly, also draws it in the center
-function drawImage(){
-    myImg = loadImage(fullsrc, () =>{
+function drawImage() {
+    myImg = loadImage(fullsrc, () => {
         ratio = myImg.width / myImg.height;
         myImg.resize(canvas_div.clientWidth, canvas_div.clientWidth / ratio);
         resizeCanvas(myImg.width, canvas_div.clientHeight);
-        image(myImg, 0,(canvas_div.clientHeight - myImg.height)/2);
+        image(myImg, 0, (canvas_div.clientHeight - myImg.height) / 2);
     })
 }
 
@@ -110,4 +113,3 @@ function selectItem(idx) {
     currentItemIdx = idx + offset;
     display();
 }
-
